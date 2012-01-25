db21de450d87c20bf25076e9edef207eef4855dc
Dong Huynh <trungdong@donggiang.com>
2012-01-25 17:34:23 +0000

    Work in progress: scaffolding between provdjango and provdm

--- provpy/provdjango/provserver/models.py
+++ provpy/provdjango/provserver/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import uuid
-from provpy import *
+from provdm.provdm.model import *
 
 PROV_RECORD_ATTRIBUTES = (
     # Relations properties
@@ -23,86 +23,202 @@ PROV_RECORD_ATTRIBUTES = (
     (101, u'prov:asserter'),
 ) 
 
-class ProvDJRecord(models.Model):
+class PDNamespace(models.Model):
+    prefix = models.CharField(max_length=255, db_index=True)
+    uri  = models.CharField(max_length=255, db_index=True)
+
+
+class PDRecord(models.Model):
     rec_id = models.CharField(max_length=255, null=True, blank=True, db_index=True)
     rec_type = models.SmallIntegerField(choices=PROV_RECORD_TYPES, db_index=True)
-    account = models.ForeignKey('ProvDJAccount', related_name='records', null=True, blank=True, db_index=True)
-    attributes = models.ManyToManyField('self', through='RecordAttribute', symmetrical=False, related_name='references')
+    account = models.ForeignKey('PDAccount', related_name='records', null=True, blank=True, db_index=True)
+
+class Attribute(models.Model):
+    record = models.ForeignKey(PDRecord, related_name='attributes', db_index=True)
+    prov_type = models.SmallIntegerField(choices=PROV_RECORD_ATTRIBUTES, null=True, blank=True, db_index=True)
+    attr_type = models.CharField(max_length=255, null=True, blank=True)
+    value = models.CharField(max_length=255, null=True, blank=True)
+
+class PDAccount(PDRecord):
+    asserter = models.CharField(max_length=255, null=True, blank=True, db_index=True)
+    namespaces = models.ManyToManyField(PDNamespace, related_name='accounts')
     
+#    def __init__(self, **kwargs):
+#        kwargs['rec_type'] = PROV_REC_ACCOUNT
+#        PDRecord.__init__(self, **kwargs)
+        
     @staticmethod
-    def create(record, account):
-        if isinstance(record, Account):
-            pass
-        else:
-            ProvDJRecord.objects.create(rec_id=record.get_record_id(), rec_type=record.get_prov_type(), account=account)
-
-class ProvDJAccountManager(models.Manager):
-    def get_query_set(self):
-        return super(ProvDJAccountManager, self).get_query_set().filter(rec_type=PROV_REC_ACCOUNT)
+    def create(account_id, asserter_id):
+        return PDAccount.objects.create(rec_id=account_id, rec_type=PROV_REC_ACCOUNT, asserter=asserter_id)
     
-class ProvDJAccount(ProvDJRecord):
-#    objects = ProvDJAccountManager()
-    class Meta:
-        proxy = True
-    
-    def __init__(self, **kwargs):
-        kwargs['rec_type'] = PROV_REC_ACCOUNT
-        ProvDJRecord.__init__(self, **kwargs)
+    def add_namespace(self, prefix, uri):
+        namespace = PDNamespace.objects.create(prefix=prefix, uri=uri)
+        self.namespaces.add(namespace)
         
-    def set_asserter(self, asserter):
-        self.literals.create(name='prov:asserter', value=asserter, datatype='xsd:anyURI')
-    
-    @staticmethod    
-    def create(account, subaccount):
+    def add_sub_account(self, account_record):
         pass
-        
-    def get_records(self):
-        return Record.objects.filter(account=self)
     
-    def get_prov_graph(self):
-        records = self.get_records()
-        graph = PROVContainer()
-        for record in records:
-            add_ProvDM_Record(graph, record)
-            
-            
-def add_ProvDM_Record(graph, record):
-    rec_type = record.rec_type
-    # TODO Get all the record attributes here
-    if rec_type == PROV_REC_ENTITY:
-        graph.add_entity(record.rec_id)
-    elif rec_type == PROV_REC_ACTIVITY:
-        graph.add_activity(record.rec_id)
-    elif rec_type == PROV_REC_AGENT:
-        graph.add_agent(record.rec_id)
-    elif rec_type == PROV_REC_ANNOTATION:
-        graph.add_note(record.rec_id)
+    def get_namespaces(self):
+        results = {}
+        for namespace in self.namespaces.all():
+            results[namespace.prefix] = namespace.uri
+        return results
+        
+    def get_PROVContainer(self):
+        return build_PROVContainer(self)
+    
+
+#class PDNamespace(models.Model):
+#    rec_id = models.CharField(max_length=255, db_index=True)
+#    uri  = models.CharField(max_length=255, db_index=True)
+#
+#class PDIdentifier(models.Model):
+#    name = models.CharField(max_length=255, db_index=True)
+#    namespace = models.ForeignKey(PDNamespace, related_name='identifiers', null=True, blank=True)
+#    
+#    @staticmethod
+#    def create(identifier):
+#        ids = PDIdentifier.objects.filter(name=identifier) 
+#        if ids.exist():
+#            return ids[0]
+#        else:
+#            return PDIdentifier.objects.create(name=identifier)
+#
+#class PDRecord(models.Model):
+#    rec_id = models.ForeignKey(PDIdentifier, related_name='records', null=True, blank=True)
+#    rec_type = models.SmallIntegerField(choices=PROV_RECORD_TYPES, db_index=True)
+#    account = models.ForeignKey('PDAccount', related_name='records', null=True, blank=True, db_index=True)
+#    attributes = models.ManyToManyField('self', through='RecordAttribute', symmetrical=False, related_name='references')
+#    
+#    @staticmethod
+#    def create(record, account):
+#        if isinstance(record, Account):
+#            pass
+#        else:
+#            PDRecord.objects.create(rec_id=record.get_record_id(), rec_type=record.get_prov_type(), account=account)
+#
+#class PDAccount(PDRecord):
+#    asserter = models.ForeignKey(PDIdentifier, related_name='accounts', null=True, blank=True)
+#    namespaces = models.ManyToManyField(PDNamespace, related_name='accounts')
+#    
+#    def __init__(self, **kwargs):
+#        kwargs['rec_type'] = PROV_REC_ACCOUNT
+#        PDRecord.__init__(self, **kwargs)
+#        
+##    def set_asserter(self, asserter):
+##        self.literals.create(name='prov:asserter', value=asserter, datatype='xsd:anyURI')
+#    @staticmethod
+#    def create(account_id, asserter):
+#        if isinstance(account_id, PDIdentifier):
+#            identifier = account_id
+#        else:
+#            identifier = PDIdentifier.create(account_id)
+#        if isinstance(asserter, PDIdentifier):
+#            asserter_id = asserter
+#        else:
+#            asserter_id = PDIdentifier.create(asserter)
+#        return PDAccount.objects.create(rec_id=identifier, asserter=asserter_id)
+#    
+#    @staticmethod    
+#    def create_sub_account(pd_account, subaccount):
+#        pass
+#        
+#    def get_records(self):
+#        return PDRecord.objects.filter(account=self)
+#    
+#    def get_prov_graph(self):
+#        records = self.get_records()
+#        graph = PROVContainer()
+#        for record in records:
+#            add_ProvDM_Record(graph, record)
+#            
+#            
+#def add_ProvDM_Record(graph, record):
+#    rec_type = record.rec_type
+#    # TODO Get all the record attributes here
+#    if rec_type == PROV_REC_ENTITY:
+#        graph.add_entity(record.rec_id)
+#    elif rec_type == PROV_REC_ACTIVITY:
+#        graph.add_activity(record.rec_id)
+#    elif rec_type == PROV_REC_AGENT:
+#        graph.add_agent(record.rec_id)
+#    elif rec_type == PROV_REC_ANNOTATION:
+#        graph.add_note(record.rec_id)
+#    else:
+#        # Unsupported type
+#        # TODO Raise an error here
+#        pass
+#    
+#class RecordAttribute(models.Model):
+#    record = models.ForeignKey(PDRecord, related_name='from_records', db_index=True)
+#    attribute = models.ForeignKey(PDRecord, related_name='to_records')
+#    prov_type = models.SmallIntegerField(choices=PROV_RECORD_ATTRIBUTES, db_index=True)
+#
+#class LiteralAttribute(models.Model):
+#    record = models.ForeignKey(PDRecord, related_name='literals', db_index=True)
+#    name = models.CharField(max_length=255)
+#    value = models.CharField(max_length=255)
+#    datatype = models.ForeignKey(PDIdentifier, related_name='literals', null=True, blank=True)
+#    
+
+def create_record(prov_record, account, record_map):
+    prov_type = prov_record.get_prov_type()
+    record_id = prov_record.get_record_id()
+    record_uri = record_id.uri() if record_id is not None else None
+    if prov_type <> PROV_REC_ACCOUNT:
+        pdrecord = PDRecord.objects.create(rec_id=record_uri, rec_type=prov_type, account=account)
+        record_map[prov_record] = pdrecord
     else:
-        # Unsupported type
-        # TODO Raise an error here
-        pass
-    
-class RecordAttribute(models.Model):
-    record = models.ForeignKey(ProvDJRecord, related_name='from_records', db_index=True)
-    attribute = models.ForeignKey(ProvDJRecord, related_name='to_records')
-    prov_type = models.SmallIntegerField(choices=PROV_RECORD_ATTRIBUTES, db_index=True)
-
-class LiteralAttribute(models.Model):
-    record = models.ForeignKey(ProvDJRecord, related_name='literals', db_index=True)
-    name = models.CharField(max_length=255)
-    value = models.CharField(max_length=255)
-    datatype = models.CharField(max_length=255)
-    
-def save_records(prov_graph):
-    account_id = uuid.uuid4()
-    account = ProvDJAccount.objects.create(rec_id=account_id)    
-    account.set_asserter('#me')
-    
-    records = prov_graph.get_records()
-    for record in records:
-        ProvDJRecord.create(record, account)
+        asserter_uri = prov_record.get_asserter().uri()
+        pdrecord = PDAccount.objects.create(rec_id=record_id, rec_type=prov_type, account=account, asserter=asserter_uri)
+        record_map[prov_record] = pdrecord
+        save_account(pdrecord, prov_record.get_records(), record_map)
         
-def test_model():
-    from provpyexample_Elements import examplegraph
-    save_records(examplegraph)
+    # TODO add all attributes here
+    return pdrecord
 
+def save_account(account, records, record_map):
+    for record in records:
+        if record not in record_map:
+            create_record(record, account, record_map)
+
+def save_records(prov_graph):
+    account_id = uuid.uuid4().urn
+    
+    account = PDAccount.create(account_id, '#me')    
+    namespaces = prov_graph.get_namespaces()
+    for (prefix,uri) in namespaces.iteritems():
+        account.add_namespace(prefix, uri)
+    default_namespace_uri = prov_graph.get_default_namespace()
+    if default_namespace_uri:
+        account.add_namespace('$', default_namespace_uri)
+     
+    records = prov_graph.get_records()
+    record_map = {}
+    save_account(account, records, record_map)
+    
+def build_PROVContainer(account):
+    graph = PROVContainer()
+    namespaces = account.get_namespaces()
+    for (prefix, uri) in namespaces.iteritems():
+        if prefix == '$':
+            graph.set_default_namespace(uri)
+        else:
+            graph.add_namespace(prefix, uri)
+    
+    record_map = {}
+    records = account.records.order_by('rec_type')
+    for record in records:
+        rec_type = record.rec_type
+        rec_id = record.rec_id
+        
+        if rec_type == PROV_REC_ACCOUNT:
+            # Do something special with account
+            pass
+        elif rec_type == PROV_REC_ENTITY:
+            graph.add_entity(rec_id)
+        elif rec_type == PROV_REC_ACTIVITY:
+            graph.add_activity(rec_id)
+            
+    return graph
+    
--- provpy/provdjango/provserver/views.py
+++ provpy/provdjango/provserver/views.py
@@ -1,11 +1,14 @@
-from models import *
-import simplejson
+from models import PDAccount, PDRecord
 from django.http import HttpResponse
 
 def get_prov_json(request):
+#    from provdjango.provserver.test.testModel import Test
+#    graph = Test.build_prov_graph()
+#    save_records(graph)
+    
     if 'id' in request.GET:
         entity_id = request.GET.get('id')
-        records = Record.objects.filter(rec_id=entity_id)
+        records = PDRecord.objects.filter(rec_id=entity_id)
         accounts = set()
         for record in records:
             accounts.add(record.account)
@@ -14,7 +17,7 @@ def get_prov_json(request):
             return HttpResponse(content='{id : %s}' % entity_id, mimetype='application/json')
         return HttpResponse(content='{Not found}', mimetype='application/json')
     else:
-        accounts = ProvDJAccount.objects.all()
-        
-        return HttpResponse(content=accounts[0].to_provJSON(), mimetype='application/json')
+        account = PDAccount.objects.get()
+        graph = account.get_PROVContainer()
+        return HttpResponse(content=graph.to_provJSON(), mimetype='application/json')
 
