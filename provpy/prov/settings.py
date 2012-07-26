dca5986047e8d7ef40c25666553f9bef8d4540f3
Trung Dong Huynh <tdh@ecs.soton.ac.uk>
2012-07-26 06:24:45 -0700

    Reinstated the default DB settings

--- provpy/prov/settings.py
+++ provpy/prov/settings.py
@@ -13,29 +13,29 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASES = {
+'default': {
+   'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+   'NAME': os.path.join(PROJECT_DIR, 'db.sqlite'),                      # Or path to database file if using sqlite3.
+   'USER': '',                      # Not used with sqlite3.
+   'PASSWORD': '',                  # Not used with sqlite3.
+   'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+   'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+}
+}
+
 #===============================================================================
 # DATABASES = {
 # 'default': {
-#   'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#   'NAME': os.path.join(PROJECT_DIR, 'db.sqlite'),                      # Or path to database file if using sqlite3.
-#   'USER': '',                      # Not used with sqlite3.
-#   'PASSWORD': '',                  # Not used with sqlite3.
-#   'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#   'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#    'NAME': 'prov',                      # Or path to database file if using sqlite3.
+#    'USER': 'postgres',                      # Not used with sqlite3.
+#    'PASSWORD': '122',                  # Not used with sqlite3.
+#    'HOST': '/tmp/',                      # Set to empty string for localhost. Not used with sqlite3.
+#    'PORT': '5433',                      # Set to empty string for default. Not used with sqlite3.
 # }
 # }
 #===============================================================================
-
-DATABASES = {
-'default': {
-   'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-   'NAME': 'prov',                      # Or path to database file if using sqlite3.
-   'USER': 'postgres',                      # Not used with sqlite3.
-   'PASSWORD': '122',                  # Not used with sqlite3.
-   'HOST': '/tmp/',                      # Set to empty string for localhost. Not used with sqlite3.
-   'PORT': '5433',                      # Set to empty string for default. Not used with sqlite3.
-}
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
