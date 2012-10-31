f6ef7f313a59e7a552e3b1918c1110798a2970b6
Trung Dong Huynh <trungdong@donggiang.com>
2012-10-31 14:18:06 +0000

    Changed: setup script updated. Version 0.4.0 submitted to pypi.org (http://pypi.python.org/pypi/prov/0.4.0)

--- provpy/setup.py
+++ provpy/setup.py
@@ -1,15 +1,36 @@
 from setuptools import setup
 
+with open('README.rst') as f:
+    long_description = f.read()
+
+with open('LICENCE.txt') as f:
+    licence = f.read()
+        
 setup(
-    name='provpy',
-    version='1.1.3',
-    author='Huanjia Yang',
-    author_email='huanjiayang@hotmail.com',
-    packages=['provpy', 'provpy.model','provpy.model.examples','provpy.model.test'],
+    name='prov',
+    version='0.4.0',
+    author='Trung Dong Huynh',
+    author_email='trungdong@donggiang.com',
+    packages=['prov', 'prov.model', 'prov.persistence', 'prov.tracking', 'prov.model.test', 'prov.tracking.test'],
     scripts=[],
     url='http://github.com/trungdong/w3-prov/tree/master/provpy',
-    license='LICENSE.txt',
-    description='A Python implementation of PROV-DM data model.',
-    long_description=open('README').read(),
-    install_requires=[],
+    license=licence,
+    description='A Python implementation of PROV data model providing simple provenance tracking and persistence using Django.',
+    long_description=long_description,
+    provides='prov',
+    keywords=['provenance', 'model', 'persistence', 'tracking', 'PROV', 'PROV-DM', 'PROV-JSON'],
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Security',
+          'Topic :: System :: Logging',
+          ],
 )
