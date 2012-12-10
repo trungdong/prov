f912ba2ce4027e38b2d980852173b44a5e10f80f
Trung Dong Huynh <trungdong@donggiang.com>
2012-12-10 00:25:32 +0000

    RDF export: Added support for Any attributes RDF test: export all example graphs

--- prov/model/test/testRDF.py
+++ prov/model/test/testRDF.py
@@ -4,8 +4,10 @@ Created on Dec 7, 2012
 @author: tdh
 '''
 import unittest
-from rdflib.graph import ConjunctiveGraph
-from prov.model.test.examples import bundles1
+import logging
+import examples
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 class Test(unittest.TestCase):
@@ -17,12 +19,22 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
-
-    def testName(self):
-        prov_graph = bundles1()
-        rdf_graph = prov_graph.rdf() 
-        print rdf_graph.serialize(format="trig")
-
+    def runTestOnGraph(self, graph):
+        logger.debug('Original graph in PROV-N\n%s' % graph.get_provn())
+        rdf_graph = graph.rdf()
+        representation = "n3"  if len(list(rdf_graph.contexts())) == 1 else "trig"
+        rdf_content = rdf_graph.serialize(format=representation) 
+        logger.debug('Original graph in RDF\n%s' % rdf_content)
+        
+    def testAllExamplesRDF(self):
+        num_graphs = len(examples.tests)
+        logger.info('Testing %d provenance graphs' % num_graphs)
+        counter = 0
+        for test, graph in examples.tests:
+            counter += 1
+            logger.info('%d. Testing the %s example' % (counter, test))
+            prov_graph = graph()
+            self.runTestOnGraph(prov_graph)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
