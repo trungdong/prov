34cdfce01dcc98f3738d8036dd5ff11ace865c69
Trung Dong Huynh <trungdong@donggiang.com>
2016-10-19 15:00:04 +0100

    Skipping SVG/DOT tests if pydotplus is not installed (closes #81)

--- prov/tests/test_dot.py
+++ prov/tests/test_dot.py
@@ -8,18 +8,22 @@ from __future__ import (absolute_import, division, print_function,
 
 import unittest
 
-from prov.dot import prov_to_dot
-from prov.tests.test_model import AllTestsBase
-from prov.tests.utility import DocumentBaseTestCase
+# Skiping SVG tests if pydotplus is not installed
+from pkgutil import find_loader
+if find_loader("pydotplus") is not None:
+
+    from prov.dot import prov_to_dot
+    from prov.tests.test_model import AllTestsBase
+    from prov.tests.utility import DocumentBaseTestCase
 
 
-class SVGDotOutputTest(DocumentBaseTestCase, AllTestsBase):
-    """
-    One-way output SVG with prov.dot to exercise its code
-    """
-    def do_tests(self, prov_doc, msg=None):
-        dot = prov_to_dot(prov_doc)
-        svg_content = dot.create(format="svg")
+    class SVGDotOutputTest(DocumentBaseTestCase, AllTestsBase):
+        """
+        One-way output SVG with prov.dot to exercise its code
+        """
+        def do_tests(self, prov_doc, msg=None):
+            dot = prov_to_dot(prov_doc)
+            svg_content = dot.create(format="svg")
 
 
 if __name__ == '__main__':
