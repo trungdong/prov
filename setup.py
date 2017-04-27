a2668595788ce921fe632198eadbf5c9aff3bd23
Trung Dong Huynh <trungdong@donggiang.com>
2017-04-27 16:26:41 +0100

    Increased version number for new development

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='prov',
-    version='1.5.0',
+    version='1.5.1.dev1',
     description='A library for W3C Provenance Data Model supporting PROV-JSON, '
                 'PROV-XML and PROV-O (RDF)',
     long_description=readme + '\n\n' + history,
