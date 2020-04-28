2e19dcc12950c1886916e7e6be4e1aecac8c9158
Trung Dong Huynh <trungdong@donggiang.com>
2020-04-28 17:45:29 +0100

    Fixed #134: checking if the output stream is not expecting unicode strings

--- src/prov/serializers/provn.py
+++ src/prov/serializers/provn.py
@@ -1,12 +1,9 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-__author__ = 'Trung Dong Huynh'
-__email__ = 'trungdong@donggiang.com'
+__author__ = "Trung Dong Huynh"
+__email__ = "trungdong@donggiang.com"
 
 import io
-import logging
-logger = logging.getLogger(__name__)
 
 from prov.serializers import Serializer
 
@@ -15,6 +12,7 @@ class ProvNSerializer(Serializer):
     """PROV-N serializer for ProvDocument
 
     """
+
     def serialize(self, stream, **kwargs):
         """
         Serializes a :class:`prov.model.ProvDocument` instance to a
@@ -23,8 +21,8 @@ class ProvNSerializer(Serializer):
         :param stream: Where to save the output.
         """
         provn_content = self.document.get_provn()
-        if isinstance(stream, io.BytesIO):
-            provn_content = provn_content.encode('utf-8')
+        if not isinstance(stream, io.TextIOBase):
+            provn_content = provn_content.encode("utf-8")
         stream.write(provn_content)
 
     def deserialize(self, stream, **kwargs):
