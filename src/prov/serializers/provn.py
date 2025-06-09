ce03334d8f720244f59a86876ec53a84e803c922
Trung Dong Huynh <trungdong@donggiang.com>
2025-06-09 21:39:15 +0100

    Add type annotations in `provn.py` #143

--- src/prov/serializers/provn.py
+++ src/prov/serializers/provn.py
@@ -2,24 +2,31 @@ __author__ = "Trung Dong Huynh"
 __email__ = "trungdong@donggiang.com"
 
 import io
+from typing import Any
 
+from prov.model import ProvDocument
 from prov.serializers import Serializer
 
 
 class ProvNSerializer(Serializer):
     """PROV-N serializer for ProvDocument"""
 
-    def serialize(self, stream, **kwargs):
+    def serialize(self, stream: io.IOBase, **args: Any) -> None:
         """
         Serializes a :class:`prov.model.ProvDocument` instance to a
         `PROV-N <http://www.w3.org/TR/prov-n/>`_.
 
         :param stream: Where to save the output.
         """
-        provn_content = self.document.get_provn()
-        if not isinstance(stream, io.TextIOBase):
-            provn_content = provn_content.encode("utf-8")
-        stream.write(provn_content)
+        if self.document is None:
+            raise Exception("No document to serialize")
 
-    def deserialize(self, stream, **kwargs):
+        provn_content = self.document.get_provn()
+        stream.write(
+            provn_content
+            if isinstance(stream, io.TextIOBase)
+            else provn_content.encode("utf-8")
+        )
+
+    def deserialize(self, stream: io.IOBase, **args: Any) -> ProvDocument:
         raise NotImplementedError
